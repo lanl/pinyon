2eee178ad6496ad03e35ec82493e4eb4a17c1e9f
Logan Ward <ward.logan.t@gmail.com>
2016-07-05 16:49:55 -0600

    Fixed tests for MDCS extractor

--- tests/extract/test_mdcs.py
+++ tests/extract/test_mdcs.py
@@ -8,7 +8,7 @@ class TestMDCSUtilities(unittest.TestCase):
     test_xml = xmltodict.parse(open(os.path.join('test-files', '1971jac2-0.xml')))
 
     def test_extractor(self):
-        flat = ExtractorFlattener(['HardnessMeasurement', 'Hardness', 'measurement-type'], \
+        flat = ExtractorFlattener(location=['HardnessMeasurement', 'Hardness', 'measurement-type'], \
                                   name='HardnessType', description="Hi Logan!")
         val = flat.extract_data(self.test_xml)
         self.assertEquals('rockwell a', val)
@@ -17,11 +17,11 @@ class TestMDCSUtilities(unittest.TestCase):
 
     def test_physical_value_extractor(self):
         # No unit conversion
-        flat = PhysicalQuantityExtractorFlattener(['HardnessMeasurement', 'AgingCondition', 'temperature'])
+        flat = PhysicalQuantityExtractorFlattener(location=['HardnessMeasurement', 'AgingCondition', 'temperature'])
         self.assertEquals(25, flat.extract_data(self.test_xml))
 
         # With unit conversion
-        flat = PhysicalQuantityExtractorFlattener(['HardnessMeasurement', 'AgingCondition', 'temperature'], units='kelvin')
+        flat = PhysicalQuantityExtractorFlattener(location=['HardnessMeasurement', 'AgingCondition', 'temperature'], units='kelvin')
         self.assertEquals(25+273.15, flat.extract_data(self.test_xml))
 
     def test_element_fraction(self):
@@ -35,26 +35,26 @@ class TestMDCSUtilities(unittest.TestCase):
         )
 
         # mole percent
-        flat = ElementFractionFlattener([], element='Na', mass=False, fraction=False)
+        flat = ElementFractionFlattener(location=[], element='Na', mass_units=False, fraction=False)
         self.assertAlmostEqual(50, flat.extract_data(value), 2)
 
         # mole fraction
-        flat = ElementFractionFlattener([], element='Na', mass=False, fraction=True)
+        flat = ElementFractionFlattener(location=[], element='Na', mass_units=False, fraction=True)
         self.assertAlmostEqual(0.5, flat.extract_data(value), 2)
 
         # mass fraction
-        flat = ElementFractionFlattener([], element='Na', mass=True, fraction=True)
+        flat = ElementFractionFlattener(location=[], element='Na', mass_units=True, fraction=True)
         self.assertAlmostEqual(0.393372, flat.extract_data(value), 4)
 
         # mole fraction, but assuming that data is in mass fraction
         value['quantityUnit'] = 'mass percent'
         value['constituent'][0]['amount'] = 50
         value['constituent'][1]['amount'] = 50
-        flat = ElementFractionFlattener([], element='Na', mass=False, fraction=False)
+        flat = ElementFractionFlattener(location=[], element='Na', mass_units=False, fraction=False)
         self.assertAlmostEqual(60.6628, flat.extract_data(value), 4)
 
         # Test out on the test_xml
-        flat = ElementFractionFlattener(['HardnessMeasurement', 'NominalComposition'], 'Nb')
+        flat = ElementFractionFlattener(location=['HardnessMeasurement', 'NominalComposition'], element='Nb', mass_units=True)
         self.assertAlmostEqual(4.5, flat.extract_data(self.test_xml), 2)
 
     def test_composition_printer(self):
@@ -68,22 +68,22 @@ class TestMDCSUtilities(unittest.TestCase):
         )
 
         # Mass percent, no units, no base element
-        flat = CompositionPrinterFlattener([], mole_percent=False)
+        flat = CompositionPrinterFlattener(location=[], mole_percent=False)
         self.assertEquals("Na50.0Cl50.0", flat.extract_data(value))
 
         # Mole percent, no units, no base element
-        flat = CompositionPrinterFlattener([], mole_percent=True)
+        flat = CompositionPrinterFlattener(location=[], mole_percent=True)
         self.assertEquals("Na60.7Cl39.3", flat.extract_data(value))
 
         # Mole percent, no units, base element=Na
-        flat = CompositionPrinterFlattener([], mole_percent=True, base_element='Na')
+        flat = CompositionPrinterFlattener(location=[], mole_percent=True, base_element='Na')
         self.assertEquals("Na-39.3Cl", flat.extract_data(value))
 
         # Mole percent, units, base element=Na
-        flat = CompositionPrinterFlattener([], mole_percent=True, base_element='Na', print_units=True)
+        flat = CompositionPrinterFlattener(location=[], mole_percent=True, base_element='Na', print_units=True)
         self.assertEquals("Na-39.3at%Cl", flat.extract_data(value))
 
         # Test on example record
-        flat = CompositionPrinterFlattener(['HardnessMeasurement', 'NominalComposition'],
+        flat = CompositionPrinterFlattener(location=['HardnessMeasurement', 'NominalComposition'],
                                            base_element='U', mole_percent=False, print_units=True)
         self.assertEquals('U-4.5wt%Nb', flat.extract_data(self.test_xml))
