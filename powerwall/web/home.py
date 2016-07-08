d757f29f90f0671d7ce5a76578d94e9b26d0c9c3
Logan Ward <ward.logan.t@gmail.com>
2016-07-08 09:24:22 -0600

    Added home page for PowerWall

---
+++ powerwall/web/home.py
@@ -0,0 +1,18 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+from ..extract import BaseExtractor
+
+
+@view_config(route_name='home', renderer='template/home.pt')
+def home(request):
+    """Home page"""
+
+    # Get the names of the extractors
+    names = [ x.name for x in BaseExtractor.objects ]
+    return {'names': names}
+
+
+def includeme(config):
+    """Used to perform includes for routes"""
+    config.add_route('home', '/')
+    config.add_route('load', '/load')
