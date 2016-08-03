10eac057fac8f8e6cb9166f839e74a3cbe5fb7d4
Logan Ward <ward.logan.t@gmail.com>
2016-08-03 17:34:41 -0600

    Refactored the jupyter notebook
    Moved the code for running the stuff outside

--- pinyon/transform/jupyter.py
+++ pinyon/transform/jupyter.py
@@ -10,6 +10,62 @@ from pinyon.utility import WorkflowTool
 from wtforms import fields as wtfields
 
 
+def run_notebook(notebook, inputs, settings):
+    """Run the notebook
+
+    :param notebook: string, notebook file contents
+    :param inputs: dict, input data to be sent to the notebook
+    :return: Output from notebook
+    """
+
+    # Parse the notebook
+    nb = nbformat.reads(notebook, nbformat.NO_CONVERT)
+    add_data(nb, inputs, settings)
+
+    # Run the notebook
+    ep = ExecutePreprocessor(timeout=-1)
+    ep.preprocess(nb, {})
+
+    # Get the results
+    outputs = pickle.loads(eval(nb.cells[-1]['outputs'][0]['data']['text/plain']))
+    return nb, outputs
+
+
+def add_data(nb, inputs, settings, use_placeholder=False):
+    """Update the code used to import and export data into this notebook
+
+    :param nb: NotebookNode, notebook used to perform the code
+    :param inputs: dict, inputs provided to this nbformat
+    :param settings: dict, settings for the calculations
+    :param use_placeholder: boolean, whether to put in an input / output placeholder
+    """
+
+    # Render the code
+    if use_placeholder:
+        import_code = "# Placeholder code for inputs"
+        export_code = "# Placeholder code for outputs"
+    else:
+        import_code = "import cPickle as pickle\n" \
+                      + ("pinyon = pickle.loads(%s)\n" % repr(pickle.dumps(inputs))) \
+                      + ("settings = pickle.loads(%s)" % repr(pickle.dumps(settings)))
+        export_code = "pickle.dumps(pinyon)"
+    check_notebook(nb)
+
+    # Put them in the documents
+    nb.cells[2]['source'] = import_code
+    nb.cells[2]['metadata']['collapsed'] = True
+    nb.cells[-1]['source'] = export_code
+    nb.cells[-1]['metadata']['collapsed'] = True
+
+    # For display mode, delete the last output
+    nb.cells[-1]['outputs'] = {}
+
+
+def check_notebook(nb):
+    if "Input Cell" not in nb.cells[1]['source'] or "Output Cell" not in nb.cells[-2]['source']:
+        raise Exception('Notebook not in expected format. Please use the template')
+
+
 class JupyterNotebookTransformer(WorkflowTool):
     """Uses Jupyter notebook to perform a certain optimization step"""
 
@@ -91,54 +147,14 @@ class JupyterNotebookTransformer(WorkflowTool):
         inputs = dict(other_inputs)
         inputs['data'] = data
 
-        # Parse the notebook
-        nb = nbformat.reads(self.notebook, nbformat.NO_CONVERT)
-        self._add_data(nb, inputs)
-
         # Run the notebook
-        ep = ExecutePreprocessor(timeout=-1)
-        ep.preprocess(nb, {})
-
-        # Get the results
-        outputs = pickle.loads(eval(nb.cells[-1]['outputs'][0]['data']['text/plain']))
+        nb, outputs = run_notebook(self.notebook, inputs, self.calc_settings)
         data = outputs['data']
         del outputs['data']
 
         # Save the notebook
         self.notebook = str(nbformat.writes(nb))
         return data, outputs
-
-    def _add_data(self, nb, inputs, use_placeholder=False):
-        """Update the code used to import and export data into this notebook
-
-        :param nb: NotebookNode, notebook used to perform the code
-        :param input: dict, inputs provided to this nbformat
-        :param use_placeholder: boolean, whether to put in an input / output placeholder
-        """
-
-        # Render the code
-        if use_placeholder:
-            import_code = "# Placeholder code for inputs"
-            export_code = "# Placeholder code for outputs"
-        else:
-            import_code = "import cPickle as pickle\n" \
-                        + ("pinyon = pickle.loads(%s)\n" % repr(pickle.dumps(inputs))) \
-                        + ("settings = pickle.loads(%s)" % repr(pickle.dumps(self.calc_settings)))
-            export_code = "pickle.dumps(pinyon)"
-        self.check_notebook(nb)
-
-        # Put them in the documents
-        nb.cells[2]['source'] = import_code
-        nb.cells[2]['metadata']['collapsed'] = True
-        nb.cells[-1]['source'] = export_code
-        nb.cells[-1]['metadata']['collapsed'] = True
-
-        # For display mode, delete the last output
-        nb.cells[-1]['outputs'] = {}
-
-    def check_notebook(self, nb):
-        if "Input Cell" not in nb.cells[1]['source'] or "Output Cell" not in nb.cells[-2]['source']:
-            raise Exception('Notebook not in expected format. Please use the template')
 
     def load_workbook(self, f):
         """Load a workbook
@@ -164,7 +180,7 @@ class JupyterNotebookTransformer(WorkflowTool):
         """
         # Make sure it is in the correct format
         nb = nbformat.reads(nb_str, nbformat.NO_CONVERT)
-        self.check_notebook(nb)
+        check_notebook(nb)
 
         # Set and leave
         self.notebook = nb_str
@@ -182,7 +198,7 @@ class JupyterNotebookTransformer(WorkflowTool):
 
         # Add data to the notebook
         nb = nbformat.reads(self.notebook, nbformat.NO_CONVERT)
-        self._add_data(nb, self.get_input())
+        add_data(nb, self.get_input(), self.calc_settings)
 
         # Write the notebook
         notebook = nbformat.writes(nb, nbformat.NO_CONVERT)
